891cffedde213176b25c8e0d7f2434aa9b60e381
Luci606 <94063743+Luci606@users.noreply.github.com>
2021-12-26 19:22:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function Board()
+{
+  return(
+    <div id='board'>
+      <div id='player2'>
+        <button id='discardedCardsP2'>Odrzucone karty</button>
+        <button id='activeCardP2'>Aktywna karta</button>
+        <div id='pointsP2'>Punkty:</div>
+      </div>
+      <div id='main'>
+        <div id='mainField'>Główne pole</div>
+        <button id='deck'>Talia</button>
+
+      </div>
+      <div id='player1'>
+        <button id='discardedCardsP1'>Odrzucone karty</button>
+        <button id='activeCardP1'>Aktywna karta</button>
+        <div id='pointsP1'>Punkty:</div>
+      </div>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Board />
   </React.StrictMode>,
   document.getElementById('root')
 );
